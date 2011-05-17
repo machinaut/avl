3e065f5e79d8fcbb5740fc18f654b2cdaecb689d
Alex Ray <ajray@ncsu.edu>
2011-05-17 12:41:54 -0400

    add surface parsing to python input file reader

--- src/input-parser.py
+++ src/input-parser.py
@@ -2,7 +2,9 @@
 # input-parser.py - parser for AVL input files using regular expressions
 # Alex Ray <ajray@ncsu.edu> 2011
 #      Notes: (and differences)
+# Only end of line character is newline (\n), the rest are just whitespace
 # Sref,Cref,Bref won't default to 1.0 if given a negative input
+# No max number of surfaces
 import re
 header = re.compile(r"""(?sx)         # re.VERBOSE and re.DOTALL
   (?P<title>[^\n]*)               \s+ # Title
@@ -16,10 +18,14 @@ header = re.compile(r"""(?sx)         # re.VERBOSE and re.DOTALL
   """)
 surface = re.compile(r"""(?sx)        # re.VERBOSE and re.DOTALL
   (?P<surf>SURF\S*)               \s+ # keyword SURFACE
-  """
+  (?P<title>[^\n]*)               \s+ # surface title
+  (?P<params>\S+[ \t]+\S+([ \t]+\S+[ \t]+\S+)?) \s*  # surface parameters
+              # Nchord  Cspace [ Nspan Sspace ]
+  (?P<remainder>.*)                   # Remainder
+  """)
 
-class plane:
-  """ plane is a flight configuration (airplane) in AVL """
+class Plane:
+  """ Plane is a flight configuration (airplane) in AVL """
   def __init__(self):
     title = ''
 #    title, mach,  cdp   = None, None, None
@@ -35,12 +41,13 @@ if __name__ == "__main__":
   if len(sys.argv) < 2:
     print "Usage: ./input-parser.py <filename>"
     exit(1)
-  p = plane()
+  p = Plane()
   s = open(sys.argv[1]).read() # read whole input file
   s = re.sub(r'(\#|!)[^\n]*\n',r'',s) # parse out comments
   m = header.match(s)
   if m is None: sys.exit("Error: Failed to match header")
   d = m.groupdict() # match header
+  print 'Configuration:', d['title']
   p.title = d['title']
   p.mach  = float(d['mach'])
   p.iysym, p.izsym       = [cmp(float(i),0) for i in d['isym'].split()]
@@ -49,18 +56,44 @@ if __name__ == "__main__":
   p.xref, p.yref, p.zref = [float(i) for i in d['sref'].split()]
   try: p.cdp = float(d['cdp']) # take care of optional CDp
   except ValueError: d['remainder'] = d['cdp']+d['remainder'] ; p.cdp = 0.0
-  s = d['remainder']
+  s = d['remainder'].upper() # convert remainder to all upper case
+  # XXX - stupid random initialization shit to make this work
+  isurf,nsurf,ibody = 0,0,0
+  lscomp = {}
+  lfwake,lfalbe,lfload = {},{},{}
   while s != '':
     m = surface.match(s)
-    if m is not None:
+    if m is not None: # matched 'SURF'
+      d = m.groupdict()
+      # new surface is about to start
       if isurf != 0:
-        #makesurf()
+        # old surface is still active
+        #makesurf() # TODO
         if ldupl: pass #sdupl()
         isurf = 0
       if ibody != 0:
-        #makebody()
+        # old body is still active, so build it before finishing
+        #makebody() # TODO
         if ldupl: pass #bdupl()
         ibody = 0
+      # new surface; isurf==0 denotes surface accumulation is active
+      nsurf = nsurf + 1
+      isurf = nsurf
+      # default surface component index is just the surface number
+      lscomp[isurf] = isurf # XXX wtf is lscomp?
+      # clear indices for accumulation TODO probably not necessary
+      nsec, isec = 0, 0
+      # set up surface defaults
+      ldupl, lhinge = False, False
+      # assume this will be a conventional loaded surface
+      lfwake[isurf],lfalbe[isurf],lfload[isurf] = True, True, True
+      xyzcal, xyztran = [1.0,1.0,1.0], [0.0,0.0,0.0]
+      addinc, ncvar = 0.0, 0
+      # XXX holy shit it takes this long to get to actual surface parsing
+      print "Building surface:", d['title']
+      if len(d['params'].split()) == 2:
+        nchord,cspace = [float(i) for i in d['params'].split()]
+      
 
       # XXX - ended in line 262 of original ainput.f
   print p
