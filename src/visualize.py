b352ada2cd315a8518794941b819e00b679d96ec
Alex Ray <ajray@ncsu.edu>
2011-05-21 01:28:08 -0400

    visualization works with cairo too now

--- src/visualize.py
+++ src/visualize.py
@@ -15,14 +15,17 @@ class Section:
 
 class Naca:
   """ Class for NACA airfoils """
+  # TODO: Support 5+ digit NACA airfoils
+  # TODO: Support caching the upper/lower/etc computations
   # Reference: http://en.wikipedia.org/wiki/NACA_airfoil
   # Reference: http://people.clarkson.edu/~pmarzocc/AE429/The%20NACA%20airfoil%20series.pdf
-  def __init__(self,naca=2415):     # intelligent
-    self.naca = '%04d' % int(naca)  # convert to a string
-    naca   = self.naca
+  def __init__(self,naca='2415'):   # intelligent
+    self.naca = naca = str(naca)    # convert to a string
     self.t = float(naca[3:])/100.0  # maximum thickness as ratio of chord
     self.m = float(naca[0])/100.0   # maximum camber as a ratio of chord
     self.p = float(naca[1])/10.0    # location of maximum camber along chord
+  def __str__(self):
+    return 'NACA '+self.naca
   def mean(self,x):
     """ calculate the mean line value for a given x coordinate along chord """
     t,m,p = self.t,self.m,self.p
@@ -81,20 +84,47 @@ class Naca:
     plt.show()
   def draw(self):
     """ draw airfoil with cairo """
+    # TODO separate the computation from the drawing
     import numpy as np
-    import cairo
-    size=500
-    surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, size,size)
-    ctx = cairo.Context (surface) # context
-    #ctx.scale (size, size) # Normalizing the canvas
-    ctx.set_source_rgb (0.7, 0.9, 0.8) # Solid color
-    ctx.fill() # background
-    x = np.arange(0.,1.001,0.001) # 1000 points along chord
-    for i in [self.upper(i) for i in x]: ctx.line_to(i[0]*.8+.1,i[1]*.8+.1)
-    ctx.set_source_rgb (0.3, 0.2, 0.5) # Solid color
-    ctx.set_line_width(0.01)
-    ctx.stroke() # draw line
-    surface.write_to_png ("example.png") # Output to PNG
+    import cairo, cStringIO
+    from pyglet import window, image
+    size=1000
+    # pyglet
+    win = window.Window(width=size,height=size/5)
+    # cairo
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, size,size/5)
+    ctx = cairo.Context(surface) # context
+    ctx.scale(size,size)
+    ctx.translate(.1,.1)
+    #ctx.translate(size/2, size/2) # Normalizing the canvas
+    # wait for trapclose
+    while not win.has_exit:
+      # cairo
+      ctx.set_source_rgb (0.7, 0.9, 0.8) # Solid color
+      ctx.paint() # background
+      ctx.set_line_width(1./size)
+      ctx.set_source_rgb (0.3, 0.2, 0.5) # Solid color
+      # DRAW THE ACTUAL AIRFOIL
+      x = np.arange(0.,1.001,0.001) # 1000 points along chord
+      for i in [self.upper(i) for i in x]: ctx.line_to(i[0]*.8,i[1]*-.8)
+      for i in [self.lower(i) for i in x[::-1]]: ctx.line_to(i[0]*.8,i[1]*-.8)
+      ctx.close_path()
+      ctx.stroke() # draw line
+      for i in [(i,self.mean(i)) for i in x]: ctx.line_to(i[0]*.8,i[1]*-.8)
+      for i in [(i,self.mean(i)) for i in x[::-1]]: ctx.line_to(i[0]*.8,i[1]*-.8)
+      ctx.close_path()
+      ctx.stroke() # draw line
+      # make a fake file and draw to it
+      fake = cStringIO.StringIO()
+      surface.write_to_png(fake) # Output to PNG
+      fake.seek(0)
+      # pyglet
+      pic = image.load('',file=fake)
+      win.dispatch_events()
+      win.clear()
+      pic.blit(0, 0)
+      win.flip()
+
 
 if __name__ == "__main__":
   naca = Naca()
